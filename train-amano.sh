931342ea183b3efedb80c268fe3bd27b8c50a109
Michael <michaelhorvath87@gmail.com>
2017-07-13 00:40:34 -0500

    create train.amano

---
+++ train-amano.sh
@@ -0,0 +1,8 @@
+python style.py --style examples/style/amano.jpg \
+  --checkpoint-dir output/checkpoint \
+  --test examples/content/people/07.jpg \
+  --test-dir output/tests \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 500 \
+  --batch-size 2
+  --allow-different-sizes
